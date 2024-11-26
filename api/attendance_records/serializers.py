254e84cdf8cb6dc5a4c7428c4c26019369658819
eskainah <eskainah@gmail.com>
2024-11-26 06:54:15 +0200

    fix bug

--- api/attendance_records/serializers.py
+++ api/attendance_records/serializers.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Attendance, Course
+
 import json
 
 User = get_user_model()
--- api/attendance_records/views.py
+++ api/attendance_records/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Attendance, Course
-from .serializers import AttendanceSerializer
+from serializers import AttendanceSerializer
 from rest_framework import viewsets, permissions
 from django.shortcuts import get_object_or_404
 
