2b81e883c2d79dc52ad398da6745d3409338f706
eskainah <eskainah@gmail.com>
2024-11-28 22:13:21 +0200

    create disbaord component

--- src/components/DashBoard/Dashboard.jsx
+++ src/components/DashBoard/Dashboard.jsx
@@ -1,22 +1,42 @@
 import React, { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../../context/AuthContext";
+import RoleBasedContent from "./RoleBaseContent";
 
 const Dashboard = () => {
+  
   const { user, logout } = useContext(AuthContext);
 
+  if (!user) return <p>No user data available</p>;
+  
   return (
-    <div>
-      {user ? (
-        <>
-          <h1>Welcome, {user.username}</h1>
-          <p>Role: {user.role}</p>
-          <button onClick={logout}>Logout</button>
-        </>
-      ) : (
-        <p>Please log in</p>
-      )}
+    <div className="dashboard-container">
+      {/* Dashboard UI */}
+      <section className="dashboard-header">
+        <div className="logoImg"></div>
+
+        <div className="profile">
+          <div className="profile_img">
+          </div>
+          <div className="user-info">
+            <h4>{user.username}</h4> {/* Display username */}
+            <p><strong>{user.role}</strong></p> {/* Display role */}
+          </div>
+        </div>
+      </section>
+      <div className="main-section">
+      
+      <section className="dashboard-nav">
+        <button>Hello</button>
+      </section>
+
+      <section className="dashboard-main">
+        <RoleBasedContent />
+      </section>
+      
+      </div>
     </div>
   );
 };
+
 
 export default Dashboard;
