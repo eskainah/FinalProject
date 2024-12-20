80c765f0d4d6388a49fdba6088629384e11357ff
eskainah <eskainah@gmail.com>
2024-12-21 00:00:55 +0200

    deleted files

--- src/components/DashBoard/RoleBaseContent.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../context/AuthContext';
-import Card from './Card';
-
-const RoleBasedContent = () => {
-  
-  }
-
-export default RoleBasedContent;
